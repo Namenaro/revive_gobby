8b084e424d09c616e4cbe87dae3540f952ce3413
neuro <nanenaro@gmail.com>
2021-01-08 17:54:25 +0300

    work..

--- selector_wizard/show_interaction.py
+++ selector_wizard/show_interaction.py
@@ -4,9 +4,11 @@ from GAN_wizard.utils import feed_np_batch_to_discriminator
 from np_datasets_wizard.utils import one_patient_to_np
 
 import numpy as np
+import matplotlib.pyplot as plt
+
 
 def get_all_signal_patches_for_every_u(u_list, model, selector, json_data):
-    num_leads = model.num_leads
+    num_leads = model.num_channels
     patch_len = model.patch_len
 
     signals_indexed_by_u = []
@@ -20,7 +22,7 @@ def get_all_signal_patches_for_every_u(u_list, model, selector, json_data):
 
         for coord in selector[id].keys():
             for i in range(len(u_list)):  # i is common index for u_list and signals_indexed_by_u
-                center_point = coord + u_list[i]
+                center_point = int(coord) + int(u_list[i])
                 cutted_patch = cut_from_signal(np_signal, patch_len, center_point)
                 if cutted_patch is None:
                     pass
@@ -28,14 +30,24 @@ def get_all_signal_patches_for_every_u(u_list, model, selector, json_data):
                     signals_indexed_by_u[i].append(cutted_patch)
     return signals_indexed_by_u
 
+
 def print_summary(signals_indexed_by_u, u_list):
-    print ("Shifts and sizes of sample (for the shift):")
+    print("Shifts and sizes of sample (for the shift):")
     for i in range(len(signals_indexed_by_u)):
         print(str(u_list[i]) + " -> " + str(len(signals_indexed_by_u[i])))
 
+
 def show_sample_by_u(u_list, validity_indexed_by_u):
     """ On X-axis are values from u_list """
-
+    for i in range(len(u_list)):
+        u = u_list[i]
+        validity_u = validity_indexed_by_u[i]
+        x = [u]*len(validity_u)
+        plt.scatter(x, validity_u, c="g", alpha=0.3)
+    plt.xlabel("Shifts")
+    plt.ylabel("Validity of model")
+    plt.legend(loc='upper left')
+    plt.show()
 
 
 def get_model_reaction_for_every_u(signals_indexed_by_u, model):
@@ -51,9 +63,9 @@ def get_model_reaction_for_every_u(signals_indexed_by_u, model):
             continue
         else:
             # TODO may be to threshold validity
-            validity, _, _= feed_np_batch_to_discriminator(model, numpy_batch_for_shift)
+            validity, _, _ = feed_np_batch_to_discriminator(model, numpy_batch_for_shift)
             validity_indexed_by_u[u_index] = validity
-    return validity_indexed_by_u[u_index]
+    return validity_indexed_by_u
 
 
 def show_interaction_selec_u_dev(u_list, selector=None, model=None):
@@ -67,3 +79,5 @@ def show_interaction_selec_u_dev(u_list, selector=None, model=None):
     validity_indexed_by_u = get_model_reaction_for_every_u(signals_indexed_by_u, model)
     show_sample_by_u(u_list, validity_indexed_by_u)
 
+if __name__ == "__main__":
+    show_interaction_selec_u_dev(u_list=[-10, 10, 20], selector=None, model=None)
